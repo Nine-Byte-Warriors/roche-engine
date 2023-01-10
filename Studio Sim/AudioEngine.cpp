ecccedaed5ea0cdd9ea17d01720032c0959ca688
Juliusz Kaczmarek <57526276+juliusz-kaczmarek@users.noreply.github.com>
2023-01-10 18:09:34 +0000

    A mess
    Don't touch unless you want to have 999+ errors (or something of sorts, because its literally mid-way through work) and headaches

--- Studio Sim/AudioEngine.cpp
+++ Studio Sim/AudioEngine.cpp
@@ -19,7 +19,7 @@ AudioEngine* AudioEngine::GetInstance()
 	return m_pAudioEngineInstance;
 }
 
-void AudioEngine::Initialize()
+void AudioEngine::Initialize(int numberOfSFXSourceVoices, int numberOfMusicSourceVoices)
 {
 	HRESULT hr;
 
@@ -32,12 +32,31 @@ void AudioEngine::Initialize()
 	if (FAILED(hr = m_pXAudio2->CreateMasteringVoice(&m_pMasterVoice))) {
 		ErrorLogger::Log(hr, "AudioEngine::Initialize: Failed to create mastering voice.");
 	}
+	
+	// Create source voice list 
+	for (int i = 0; m_vSFXSourceVoices.size() < i; i++) {
+		if (FAILED(hr = m_pXAudio2->CreateSourceVoice(&m_vSFXSourceVoices[i]->sourceVoice))) {
+			ErrorLogger::Log(hr, "Failed to CreateSourceVoice");
+			return hr;
+		}
+	}
 
-	ParseAudio(); // Remove later
+	// Remove Later
+	//ParseAudio(L"TestAudioFiles\\pcm-32bit-44khz-mono.wav", pSourceVoice2);
+
+	ParseAudio(L"TestAudioFiles\\piano2.wav", pSourceVoice);
+	ParseAudio(L"TestAudioFiles\\quietlaugh.wav", pSourceVoice2);
+
+
 
 }
 
-HRESULT AudioEngine::ParseAudio()
+void AudioEngine::Update(float deltaTime)
+{
+	
+}
+
+HRESULT AudioEngine::LoadAudio(std::wstring filePath)
 {
 	HRESULT hr = S_OK;
 
@@ -45,8 +64,110 @@ HRESULT AudioEngine::ParseAudio()
 	WAVEFORMATEXTENSIBLE wfx = { 0 };
 	XAUDIO2_BUFFER buffer = { 0 };
 
+
+
 	//// Open audio with CreateFile
-	WCHAR* strFileName = (WCHAR*)_TEXT("TestAudioFiles\\pcm-32bit-44khz-mono.wav");
+	//WCHAR* strFileName = (WCHAR*)TEXT("TestAudioFiles\\pcm-32bit-44khz-mono.wav");
+	const wchar_t* strFileName = filePath.c_str();
+
+	// Open file
+	HANDLE hFile = CreateFile(
+		strFileName,
+		GENERIC_READ,
+		FILE_SHARE_READ,
+		NULL,
+		OPEN_EXISTING,
+		0,
+		NULL);
+
+	if (INVALID_HANDLE_VALUE == hFile) {
+		hr = HRESULT_FROM_WIN32(GetLastError());
+		ErrorLogger::Log(hr, "AudioEngine::ParseAudio: INVALID_HANDLE_VALUE Error");
+		return hr;
+	}
+
+	if (INVALID_SET_FILE_POINTER == SetFilePointer(hFile, 0, NULL, FILE_BEGIN)) {
+		hr = HRESULT_FROM_WIN32(GetLastError());
+		ErrorLogger::Log(hr, "AudioEngine::ParseAudio: INVALID_SET_FILE_POINTER Error");
+		return hr;
+	}
+
+	// Find RIFF chunk and check the file type
+	DWORD dwChunkSize;
+	DWORD dwChunkPosition;
+	//check the file type, should be fourccWAVE or 'XWMA'
+	FindChunk(hFile, fourccRIFF, dwChunkSize, dwChunkPosition);
+	DWORD filetype;
+	ReadChunkData(hFile, &filetype, sizeof(DWORD), dwChunkPosition);
+
+	if (filetype != fourccWAVE) {
+		hr = S_FALSE;
+		ErrorLogger::Log(hr, "AudioEngine::ParseAudio: File type is not WAVE");
+		return hr;
+	}
+
+	FindChunk(hFile, fourccFMT, dwChunkSize, dwChunkPosition);
+	ReadChunkData(hFile, &wfx, dwChunkSize, dwChunkPosition);
+
+	//fill out the audio data buffer with the contents of the fourccDATA chunk
+	FindChunk(hFile, fourccDATA, dwChunkSize, dwChunkPosition);
+	BYTE* pDataBuffer = new BYTE[dwChunkSize];
+	ReadChunkData(hFile, pDataBuffer, dwChunkSize, dwChunkPosition);
+
+	buffer.AudioBytes = dwChunkSize;  //size of the audio buffer in bytes
+	buffer.pAudioData = pDataBuffer;  //buffer containing audio data
+	buffer.Flags = XAUDIO2_END_OF_STREAM; // tell the source voice not to expect any data after this buffer
+}
+
+HRESULT AudioEngine::PlayAudio(std::wstring fileName)
+{
+	HRESULT hr = S_OK;
+
+	// Check with the file name exists, if so, grab a buffer from it
+	for (int i = 0; m_vSFXSoundBank.size() < i; i++) {
+		if (fileName == m_vSFXSoundBank[i]->fileName) {
+			// Check whether the sound can be allocated into any source voice
+			// Basically if there is source voice without a buffer or finds the lower priority sound
+			// Replaces THE LOWEST priority sound
+			for (int j = 0; m_vSFXSourceVoices.size() < j; j++) {
+				
+			}
+
+
+
+
+
+
+		}
+	}
+
+
+	//if (FAILED(hr = sourceVoice->SubmitSourceBuffer(&buffer))) {
+	//	ErrorLogger::Log(hr, "Failed to SubmitSourceBuffer");
+	//	return hr;
+	//}
+
+	//if (FAILED(hr = sourceVoice->Start(0))) {
+	//	ErrorLogger::Log(hr, "Failed to start Source Voice");
+	//	return hr;
+	//}
+
+	return hr;
+}
+
+HRESULT AudioEngine::ParseAudio(std::wstring filePath, IXAudio2SourceVoice* sourceVoice)
+{
+	HRESULT hr = S_OK;
+
+	//// Create structures
+	WAVEFORMATEXTENSIBLE wfx = { 0 };
+	XAUDIO2_BUFFER buffer = { 0 };
+
+	
+
+	//// Open audio with CreateFile
+	//WCHAR* strFileName = (WCHAR*)TEXT("TestAudioFiles\\pcm-32bit-44khz-mono.wav");
+	const wchar_t* strFileName = filePath.c_str();
 
 	// Open file
 	HANDLE hFile = CreateFile(
@@ -95,6 +216,31 @@ HRESULT AudioEngine::ParseAudio()
 	buffer.AudioBytes = dwChunkSize;  //size of the audio buffer in bytes
 	buffer.pAudioData = pDataBuffer;  //buffer containing audio data
 	buffer.Flags = XAUDIO2_END_OF_STREAM; // tell the source voice not to expect any data after this buffer
+
+	// Put the buffer into container here
+	//
+	//vector?
+	//
+	//
+
+	// TEST
+
+
+	if (FAILED(hr = m_pXAudio2->CreateSourceVoice(&sourceVoice, (WAVEFORMATEX*)&wfx))) {
+		ErrorLogger::Log(hr, "Failed to CreateSourceVoice");
+		return hr;
+	}
+
+	if (FAILED(hr = sourceVoice->SubmitSourceBuffer(&buffer))) {
+		ErrorLogger::Log(hr, "Failed to SubmitSourceBuffer");
+		return hr;
+	}
+
+	if (FAILED(hr = sourceVoice->Start(0))) {
+		ErrorLogger::Log(hr, "Failed to start Source Voice");
+		return hr;
+	}
+
 
 	return hr;
 }
--- Studio Sim/AudioEngine.h
+++ Studio Sim/AudioEngine.h
@@ -2,8 +2,10 @@
 #include <xaudio2.h>
 #include <thread>
 #include <mutex>
+#include <stdafx.h>
 
-#include <tchar.h>
+//#include <tchar.h>
+//#include <winnt.h>
 
 //#include "mmeapi.h"
 
@@ -15,6 +17,18 @@
 #define fourccXWMA 'AMWX'
 #define fourccDPDS 'sdpd'
 
+struct SoundBankFile {
+	std::wstring fileName;
+	XAUDIO2_BUFFER* buffer;
+	float volume;
+	int priority;
+};
+
+struct SourceVoiceList {
+	IXAudio2SourceVoice* sourceVoice;
+	int priority;
+};
+
 class AudioEngine {
 public:
 	AudioEngine();
@@ -24,18 +38,24 @@ public:
 
 	static AudioEngine* GetInstance();
 
-	void Initialize();
+	void Initialize(int numberOfSFXSourceVoices, int numberOfMusicSourceVoices);
+	void Update(float deltaTime);
 
 	// Bare minimum requirements
-	void LoadAudio();
-	void PlayAudio();
+	HRESULT LoadAudio(std::wstring filePath);
+	HRESULT PlayAudio(std::wstring fileName);
 	void PauseAudio();
 	void UnloadAudio();
 
-	////
-	HRESULT ParseAudio();
+	//// Loading Audio stuff
+	// TODO
+	// Allocate this stuff in some buffers container?
+	HRESULT ParseAudio(std::wstring filePath, IXAudio2SourceVoice* sourceVoice);
 	HRESULT FindChunk(HANDLE hFile, DWORD fourcc, DWORD& dwChunkSize, DWORD& dwChunkDataPosition);
 	HRESULT ReadChunkData(HANDLE hFile, void* buffer, DWORD buffersize, DWORD bufferoffset);
+
+
+
 
 	// Event System TBD if we want to even use it
 	//void HandleEvent(Event* event);
@@ -44,8 +64,19 @@ private:
 	IXAudio2* m_pXAudio2; // XAudio2 audio engine instance
 	IXAudio2MasteringVoice* m_pMasterVoice;
 
-
 	// For singleton
 	static AudioEngine* m_pAudioEngineInstance;
 	static std::mutex m_mutex;
+
+	//TEST
+	IXAudio2SourceVoice* pSourceVoice;
+	IXAudio2SourceVoice* pSourceVoice2;
+
+	std::vector<SoundBankFile*> m_vMusicSoundBank; // Music Sound Bank
+	std::vector<SoundBankFile*> m_vSFXSoundBank; // SFX Sound Bank
+
+	std::vector<SourceVoiceList*> m_pMusicSourceVoices; // Music Source Voices
+	std::vector<SourceVoiceList*> m_vSFXSourceVoices; // Channels Source Voice
+
+
 };
