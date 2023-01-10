6b0f9db10febf258ce814ab197eeb3534e6938be
Maurice Thompson-Hamilton <mauricemlth@hotmail.com>
2023-01-10 16:04:11 +0000

    remove some comments from class

--- Studio Sim/Objects/Agent.cpp
+++ Studio Sim/Objects/Agent.cpp
@@ -6,17 +6,18 @@ void Agent::Initialize( const std::shared_ptr<Transform>& transform )
 	m_fSpeed = 10.0f;
 	m_transform = transform;
 	
-	m_pStateMachine = new /*AILogic::*/AIStateMachine(this);
-	/*AILogic::*/AIState* pSeekState = m_pStateMachine->NewState(/*AILogic::*/AIStateTypes::Seek);
+	// TODO: replace with values from JSON 
+	m_pStateMachine = new AIStateMachine(this);
+	AIState* pSeekState = m_pStateMachine->NewState(AIStateTypes::Seek);
 	pSeekState->SetBounds(1.0f, 0.0f);
 	pSeekState->SetActivation(0.0f);
 	m_vecStates.push_back(pSeekState);
 	
-	/*AILogic::*/AIState* pIdleState = m_pStateMachine->NewState(/*AILogic::*/AIStateTypes::Idle);
+	AIState* pIdleState = m_pStateMachine->NewState(AIStateTypes::Idle);
 	pIdleState->SetActivation(0.0f);
 	m_vecStates.push_back(pIdleState);
 
-	/*AILogic::*/AIState* pFleeState = m_pStateMachine->NewState(/*AILogic::*/AIStateTypes::Flee);
+	AIState* pFleeState = m_pStateMachine->NewState(AIStateTypes::Flee);
 	pFleeState->SetBounds(1.0f, 0.0f);
 	pFleeState->SetActivation(1.0f);
 	m_vecStates.push_back(pFleeState);
@@ -28,7 +29,7 @@ void Agent::Update(float dt)
 {
 	m_pStateMachine->Clear();
 	
-	for (/*AILogic::*/AIState* pState : m_vecStates)
+	for (AIState* pState : m_vecStates)
 		m_pStateMachine->AddState(pState);
 	
 	m_pStateMachine->UpdateMachine(dt);
