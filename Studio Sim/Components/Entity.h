9dd881cee0fb89f88b54ddcdf8bd7c51e75a770d
CharlieMorris <m015810j@student.staffs.ac.uk>
2023-01-17 17:04:13 +0000

    Entity Editor WIP

--- Studio Sim/Components/Entity.h
+++ Studio Sim/Components/Entity.h
@@ -2,26 +2,44 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
-#include "JsonLoading.h"
+class Graphics;
+#include "EntityController.h"
+#include "Physics.h"
+#include "EventSystem.h"
+#include "ProjectileManager.h"
+#include "Collider.h"
 
-struct EntityData
-{
-	std::string name;
-	std::string texture;
-	int type;
-};
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(EntityData, name, texture, type)
-
-class Entity
+class Entity : public Listener
 {
 public:
-	Entity();
-	~Entity();
+	Entity(EntityController& entityController, int EntityNum);
+
+	void Initialize(const Graphics& gfx, ConstantBuffer<Matrices>& mat);
+	void Update(const float dt);
+
+	void AddToEvent() noexcept;
+	void HandleEvent(Event* event) override;
+
+	inline std::shared_ptr<Sprite> GetSprite() const noexcept { return m_sprite; }
+	inline std::shared_ptr<Physics> GetPhysics() const noexcept { return m_physics; }
+	inline std::shared_ptr<Transform> GetTransform() const noexcept { return m_transform; }
+	inline std::shared_ptr<ProjectileManager> GetProjectileManager() const noexcept { return m_projectileManager; }
+	inline std::shared_ptr<BoxCollider> GetCollider() const noexcept { return m_collider; };
 
 private:
-	std::string JsonFile = "Resources\\Entity\\Entity.json";
+	void SetPositionInit();
+	void SetScaleInit();
 
-	std::vector<EntityData> m_entityData;
+	int m_iEntityNum;
+
+	Vector2f* m_vPosition;
+	std::shared_ptr<Sprite> m_sprite;
+	std::shared_ptr<Physics> m_physics;
+	std::shared_ptr<Transform> m_transform;
+	std::shared_ptr<ProjectileManager> m_projectileManager;
+	std::shared_ptr<BoxCollider> m_collider;
+
+	EntityController* m_entityController;
 };
 
 #endif
