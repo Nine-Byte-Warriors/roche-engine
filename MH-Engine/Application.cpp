8f0a8daba448d78b6ae246e3f453c949ae0ab700
Kyle Robinson <w02126j@student.staffs.ac.uk>
2023-01-22 19:02:20 +0000

    Renamed project files and added debug/release builds.

--- MH-Engine/Application.cpp
+++ MH-Engine/Application.cpp
@@ -10,7 +10,7 @@ bool Application::Initialize( HINSTANCE hInstance, int width, int height )
     try
     {
         // Initialize window
-        if ( !m_renderWindow.Initialize( &m_input, hInstance, "Five Nights of Farming", "TutorialWindowClass", width, height ) )
+        if ( !m_renderWindow.Initialize( &m_input, hInstance, "Midnight Harvest -Engine-", "TutorialWindowClass", width, height ) )
 		    return false;
 
         // Initialize m_graphics
