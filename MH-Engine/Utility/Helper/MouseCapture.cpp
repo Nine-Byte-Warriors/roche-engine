accba0df8f27264039e80aac2f8b74d98b265505
Kyle ROBINSON <w021256j@student.staffs.ac.uk>
2023-01-27 13:12:03 +0000

    Mouse position in scene window now updates correctly.

--- MH-Engine/Utility/Helper/MouseCapture.cpp
+++ MH-Engine/Utility/Helper/MouseCapture.cpp
@@ -1,37 +1,14 @@
 #include "stdafx.h"
 #include "MouseCapture.h"
 
-MouseCaptureData MouseCapture::m_data = 
+#if _DEBUG
+Vector2f MouseCapture::GetGamePos(ImVec2 ivMousePos, Vector2f vEditorPos, ImVec2 ivEditorSize, ImVec2 ivSceneSize)
 {
-	new Vector2f(0.0f, 0.0f), 
-	new Vector2f(0.0f, 0.0f), 
-	new Vector2f(0.0f, 0.0f), 
-	new Vector2f(0.0f, 0.0f) 
-};
-
-Vector2f MouseCapture::GetGamePos(ImVec2 ivMousetPos, Vector2f vEditorPos, ImVec2 ivEditorSize, ImVec2 ivSceneSize)
-{
-	Vector2f vMousePos = { ivMousetPos.x, ivMousetPos.y };
-	//Vector2f vEditorPos = { vEditorPos.x, vEditorPos.y };
+	Vector2f vMousePos = { ivMousePos.x, ivMousePos.y };
 	Vector2f vEditorSize = { ivEditorSize.x, ivEditorSize.y };
 	Vector2f vSceneSize = { ivSceneSize.x, ivSceneSize.y };
-	
-	m_data.vMousePos = &vMousePos;
-	m_data.vEditorPos = &vEditorPos;
-	m_data.vEditorSize = &vEditorSize;	
-	m_data.vSceneSize = &vSceneSize;
-	
-	if (MouseCapture::IsInScene(vMousePos, vEditorPos, vSceneSize))
-		return EditorPosToGamePos(vMousePos, vEditorPos, vEditorSize, vSceneSize);
-	
-	return { -1.0f, -1.0f };
-}
 
-bool MouseCapture::IsInScene(Vector2f vMousePos, Vector2f vEditorPos, Vector2f vSceneSize)
-{
-	return (vMousePos.x > vEditorPos.x && vMousePos.x < (vEditorPos.x + vSceneSize.x) &&
-		vMousePos.y > vEditorPos.y && vMousePos.y < (vEditorPos.y + vSceneSize.y))
-		? true : false;
+	return EditorPosToGamePos(vMousePos, vEditorPos, vEditorSize, vSceneSize);
 }
 
 Vector2f MouseCapture::EditorPosToGamePos(Vector2f vInputPos, Vector2f vEditorPos, Vector2f vEditorSize, Vector2f vGameSize)
@@ -42,3 +19,4 @@ Vector2f MouseCapture::EditorPosToGamePos(Vector2f vInputPos, Vector2f vEditorPo
 		(vInputPos.y - vEditorPos.y) * (vGameSize.y / vEditorSize.y)
 	);
 }
+#endif
--- MH-Engine/Utility/Helper/MouseCapture.h
+++ MH-Engine/Utility/Helper/MouseCapture.h
@@ -7,57 +7,13 @@
 
 #if _DEBUG
 #include <imgui/imgui.h>
-#endif
-
-struct MouseCaptureData
-{
-	Vector2f* vMousePos;
-	Vector2f* vEditorPos;
-	Vector2f* vEditorSize;
-	Vector2f* vSceneSize;
-
-	void DataOverlay()
-	{
-		ImGui::Text("MouseCapture");
-		ImGui::Text("MousePos: %.2f, %.2f", vMousePos-> x, vMousePos->y);
-		ImGui::Text("EditorPos: %.2f, %.2f", vEditorPos->x, vEditorPos->y);
-		ImGui::Text("EditorSize: %.2f, %.2f", vEditorSize->x, vEditorSize->y);
-		ImGui::Text("SceneSize: %.2f, %.2f", vSceneSize->x, vSceneSize->y);
-	}
-	
-	Vector2f GetGameMousePos()
-	{
-		return Vector2f
-		(
-			(vMousePos->x - vEditorPos->x) * (vSceneSize->x / vEditorSize->x),
-			(vMousePos->y - vEditorPos->y) * (vSceneSize->y / vEditorSize->y)
-		);
-	}
-};
 
 class MouseCapture : Listener
 {
 public:
-	MouseCapture() { m_data = MouseCaptureData(); AddToEvent(); };
-	~MouseCapture() {};
-
-	static Vector2f GetGamePos(ImVec2 ivMousetPos, Vector2f vEditorPos, ImVec2 ivEditorSize, ImVec2 ivSceneSize);
-	static bool IsInScene(Vector2f vMousePos, Vector2f vScenePos, Vector2f vSceneSize);
-	
-	static Vector2f EditorPosToGamePos(Vector2f vInputPos, Vector2f vEditorPos, Vector2f vEditorSize, Vector2f vGameSize);
-
-	static MouseCaptureData GetOverlayData() { return m_data; }
-	
-	void AddToEvent() noexcept { EventSystem::Instance()->AddClient(EVENTID::MousePosition, this); }
-	void HandleEvent(Event* event) noexcept override
-	{
-		if (event->GetEventID() == EVENTID::MousePosition)
-			m_data.vMousePos = (Vector2f*)event->GetData();
-	};
-	
-private:
-	static MouseCaptureData m_data;
-
+	static Vector2f GetGamePos(ImVec2 ivMousetPos, Vector2f vEditorPos, ImVec2 ivEditorSize, ImVec2 ivSceneSize);	
+	static Vector2f EditorPosToGamePos(Vector2f vInputPos, Vector2f vEditorPos, Vector2f vEditorSize, Vector2f vGameSize);	
 };
+#endif
 
 #endif // !MOUSE_CAPTURE_H
