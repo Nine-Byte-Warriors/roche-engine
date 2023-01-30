6f0cc245fa19e6d7088c2f6375a217b6c5691930
WillBennett2 <72083740+WillBennett2@users.noreply.github.com>
2023-01-30 13:55:18 +0000

    Adding Files and moving update in UIScreen

--- enc_temp_folder/b514182ca8101353636d69a520595bba/UIScreen.cpp
+++ enc_temp_folder/b514182ca8101353636d69a520595bba/UIScreen.cpp
@@ -224,32 +224,38 @@ void UIScreen::Update( const float dt, const std::vector<Widget>& widgets )
 		if (m_vImages[i].GetAction() == "Carrot Seed Packet")
 		{
 			m_vImages[i].Resolve("Resources\\Textures\\UI\\Seeds\\Carrot Seeds.png");
+			m_vImages[i].Update(dt);
 		}
 		else if (m_vImages[i].GetAction() == "Cauliflower Seed Packet")
 		{
 			m_vImages[i].Resolve("Resources\\Textures\\UI\\Seeds\\Cauliflower Seeds.png");
+			m_vImages[i].Update(dt);
 		}
 		else if (m_vImages[i].GetAction() == "Bean Seed Packet")
 		{
 			m_vImages[i].Resolve("Resources\\Textures\\UI\\Seeds\\Bean Seeds.png");
+			m_vImages[i].Update(dt);
 		}
 		else if (m_vImages[i].GetAction() == "Onion Seed Packet")
 		{
 			m_vImages[i].Resolve("Resources\\Textures\\UI\\Seeds\\Onion Seeds.png");
+			m_vImages[i].Update(dt);
 		}
 		else if (m_vImages[i].GetAction() == "Potato Seed Packet")
 		{
 			m_vImages[i].Resolve("Resources\\Textures\\UI\\Seeds\\Potato Seeds.png");
+			m_vImages[i].Update(dt);
 		}
 		else if (m_vImages[i].GetAction() == "Tomato Seed Packet")
 		{
 			m_vImages[i].Resolve("Resources\\Textures\\UI\\Seeds\\Tomato Seeds.png");
+			m_vImages[i].Update(dt);
 		}
 		else
 		{
 			m_vImages[i].Resolve("Resources\\Textures\\UI\\Board\\Board.png");
+			m_vImages[i].Update(dt);
 		}
-		m_vImages[i].Update(dt);
 	}
 
 	for ( unsigned int i = 0; i < m_vInputs.size(); i++ )
