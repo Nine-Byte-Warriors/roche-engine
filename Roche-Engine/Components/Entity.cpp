cd43a85faba4229edd8c6e14c569713e31f2a59a
Alvin Aggrey <40688105+AlvinAggrey@users.noreply.github.com>
2023-02-04 22:54:43 +0000

    Toggle Trigger from Editor

--- Roche-Engine/Components/Entity.cpp
+++ Roche-Engine/Components/Entity.cpp
@@ -41,7 +41,7 @@ void Entity::SetComponents()
 	}
 	if (m_entityController->HasCollider(m_iEntityNum))
 	{
-		bool trigger = false;
+		bool trigger = m_entityController->GetColliderTrigger(m_iEntityNum);
 		m_colliderCircle = std::make_shared<CircleCollider>(trigger, m_transform, m_iEntityNum, m_sEntityType, 32);
 		m_colliderBox = std::make_shared<BoxCollider>(trigger, m_transform, m_iEntityNum, m_sEntityType, 32, 32);
 	}
