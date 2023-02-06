420eaf6fc9aece3f25a90a01673118ccf82538e9
CharlieMorris <m015810j@student.staffs.ac.uk>
2023-02-06 14:39:43 +0000

    Now now click to shoot

--- Roche-Engine/Components/Agent.cpp
+++ Roche-Engine/Components/Agent.cpp
@@ -7,7 +7,7 @@
 
 using namespace AILogic;
 
-Agent::Agent( const std::shared_ptr<Physics>& physics ) : m_physics( physics )
+Agent::Agent(const std::shared_ptr<Physics>& physics) : m_physics(physics)
 {
 	m_fSpeed = 5.0f;
 
@@ -64,7 +64,7 @@ Agent::Agent( const std::shared_ptr<Physics>& physics ) : m_physics( physics )
 	pFireState->SetBounds(1.0f, 0.0f);
 	pFireState->SetActivation(0.0f);
 	m_mapStates.emplace(AIStateTypes::Fire, pFireState);
-	
+
 	AddToEvent();
 }
 
@@ -177,9 +177,9 @@ void Agent::SpawnControlWindow(Vector2f fGO, Vector2f fTarg) noexcept
 		ImGui::Separator();
 		ImGui::NewLine();
 
-        ImGui::Text("Enemy");
-        ImGui::Text(std::string("X: ").append(std::to_string(fGO.x)).c_str());
-        ImGui::Text(std::string("Y: ").append(std::to_string(fGO.y)).c_str());
+		ImGui::Text("Enemy");
+		ImGui::Text(std::string("X: ").append(std::to_string(fGO.x)).c_str());
+		ImGui::Text(std::string("Y: ").append(std::to_string(fGO.y)).c_str());
 
 		ImGui::NewLine();
 		float fDistance = fGO.Distance(fTarg);
@@ -211,9 +211,9 @@ void Agent::SpawnControlWindow(Vector2f fGO, Vector2f fTarg) noexcept
 		ImGui::Separator();
 		ImGui::NewLine();
 
-        ImGui::Text("Target");
+		ImGui::Text("Target");
 		ImGui::Text(std::string("X: ").append(std::to_string(fTarg.x)).c_str());
-        ImGui::Text(std::string("Y: ").append(std::to_string(fTarg.y)).c_str());
+		ImGui::Text(std::string("Y: ").append(std::to_string(fTarg.y)).c_str());
 		static int targetGroup = 0;
 		if (ImGui::RadioButton("Mouse", &targetGroup, 0))
 			m_bTargetMouse = true;
@@ -247,13 +247,13 @@ void Agent::ResetBehaviour()
 
 void Agent::AddToEvent() noexcept
 {
-	EventSystem::Instance()->AddClient(EVENTID::MousePosition, this);
+	EventSystem::Instance()->AddClient(EVENTID::MouseCameraPosition, this);
 	EventSystem::Instance()->AddClient(EVENTID::PlayerPosition, this);
 }
 
 void Agent::RemoveFromEvent() noexcept
 {
-	EventSystem::Instance()->RemoveClient(EVENTID::MousePosition, this);
+	EventSystem::Instance()->RemoveClient(EVENTID::MouseCameraPosition, this);
 	EventSystem::Instance()->RemoveClient(EVENTID::PlayerPosition, this);
 }
 
@@ -261,12 +261,17 @@ void Agent::HandleEvent(Event* event)
 {
 	switch (event->GetEventID())
 	{
-	case EVENTID::MousePosition:
+	case EVENTID::MouseCameraPosition:
+	{
 		if (m_bTargetMouse)
-			m_vTargetPos = *(Vector2f*)event->GetData();
+		{
+			m_vTargetPos = *static_cast<Vector2f*>(event->GetData());
+		}
+
 		break;
+	}
 	case EVENTID::PlayerPosition:
-		if (!m_bTargetMouse) 
+		if (!m_bTargetMouse)
 		{
 			std::pair < Sprite*, Vector2f*>* dPair = (std::pair<Sprite*, Vector2f*>*)(event->GetData());
 			m_vTargetPos = *dPair->second;
--- Roche-Engine/TileMap/TileMapPaintOnMap.cpp
+++ Roche-Engine/TileMap/TileMapPaintOnMap.cpp
@@ -34,7 +34,7 @@ int TileMapPaintOnMap::GetTileMapPos()
 
 bool TileMapPaintOnMap::IsLeftMouseDown()
 {
-	bool isMouseInPlayerArea = m_iTileX >= 0 && m_iTileX < m_iCols && m_iTileY >= 0 && m_iTileY < m_iRows;
+	bool isMouseInPlayerArea = m_iTileX >= 0 && m_iTileX < m_iCols&& m_iTileY >= 0 && m_iTileY < m_iRows;
 	if (isMouseInPlayerArea)
 	{
 		return m_bLeftMouseDown;
@@ -64,14 +64,14 @@ int TileMapPaintOnMap::GetPositionAtCoordinates(int x, int y)
 
 void TileMapPaintOnMap::AddToEvent() noexcept
 {
-	EventSystem::Instance()->AddClient(EVENTID::ImGuiMousePosition, this);
+	EventSystem::Instance()->AddClient(EVENTID::MouseCameraPosition, this);
 	EventSystem::Instance()->AddClient(EVENTID::LeftMouseClick, this);
 	EventSystem::Instance()->AddClient(EVENTID::LeftMouseRelease, this);
 }
 
 void TileMapPaintOnMap::RemoveFromEvent() noexcept
 {
-	EventSystem::Instance()->RemoveClient(EVENTID::ImGuiMousePosition, this);
+	EventSystem::Instance()->RemoveClient(EVENTID::MouseCameraPosition, this);
 	EventSystem::Instance()->RemoveClient(EVENTID::LeftMouseClick, this);
 	EventSystem::Instance()->RemoveClient(EVENTID::LeftMouseRelease, this);
 }
@@ -80,14 +80,12 @@ void TileMapPaintOnMap::HandleEvent(Event* event)
 {
 	switch (event->GetEventID())
 	{
-	case EVENTID::ImGuiMousePosition:
+	case EVENTID::MouseCameraPosition:
 	{
-		m_fCameraX = m_camera->GetPosition().x - m_camera->GetInitPosition().x + m_iStartingPosX;
-		m_fCameraY = m_camera->GetPosition().y - m_camera->GetInitPosition().y + m_iStartingPosY;
+		Vector2f mousePos2 = *static_cast<Vector2f*>(event->GetData());
 
-		Vector2f mousePos = *static_cast<Vector2f*>(event->GetData());
-		m_iTileX = (mousePos.x + m_fCameraX) / 32;
-		m_iTileY = (mousePos.y + m_fCameraY) / 32;
+		m_iTileX = (mousePos2.x + m_iStartingPosX) / 32;
+		m_iTileY = (mousePos2.y + m_iStartingPosY) / 32;
 		m_iPos = m_iTileX + m_iTileY * m_iCols;
 	}
 	break;
